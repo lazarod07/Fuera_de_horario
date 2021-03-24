544cb110ed82a0847bbac230bf326d98b8e25364
lazarod70 <cristianlazarod@gmail.com>
2021-03-24 00:38:38 -0500

    Init

---
+++ src/main/java/co/cristian/springboot/horariointerceptor/app/controller/AppController.java
@@ -0,0 +1,47 @@
+package co.cristian.springboot.horariointerceptor.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@Value("${config.horario.apertura}")
+	private Integer apertura;
+	
+	@Value("${config.horario.cierre}")
+	private Integer cierre;
+
+	@GetMapping({"/", "/index"})
+	public String index(Model model) {
+		
+		model.addAttribute("titulo", "Bienvenido al horario de atencion a clientes");
+		
+		return "index";
+		
+	}
+	
+	@GetMapping("/cerrado")
+	public String cerrado(Model model) {
+		
+		StringBuilder mensaje = new StringBuilder("Cerrado, por favor visitenos desde las ");
+		
+		mensaje.append(apertura);
+		
+		mensaje.append(" y las");
+		
+		mensaje.append(cierre);
+		
+		mensaje.append(" hrs.  Gracias");
+		
+		model.addAttribute("mensaje", mensaje);
+		
+		model.addAttribute("titulo", "Fuera del horario de atencion");
+		
+		return "cerrado";
+		
+	}
+	
+}
